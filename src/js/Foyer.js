2af72839c1bf7c900cd588f32bf03c0696dd635d
Guido Schmidt <git@guidoschmidt.cc>
2020-09-21 23:11:05 +0200

    EDIT/src/js/Foyer.js: change title from Lobby to Foyer

--- src/js/Foyer.js
+++ src/js/Foyer.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 import { get } from "./api/api.js";
 
-const Lobby = ({ exhibitions }) => {
+export default ({ exhibitions }) => {
   return (
     <div>
-      <h1>Lobby</h1>
+      <h1>Foyer</h1>
       <h2>Exhibition Rooms:</h2>
       {exhibitions.map(entry => {
         return (
@@ -19,5 +19,3 @@ const Lobby = ({ exhibitions }) => {
     </div>
   );
 };
-
-export default Lobby;
