005523e01b5f12a41b0c99bcc8e20ca42473f0af
Guido Schmidt <git@guidoschmidt.cc>
2020-09-22 08:18:09 +0200

    WIP: parsing + layouting content blocks

--- src/js/layouts/ExhibitionLayout.js
+++ src/js/layouts/ExhibitionLayout.js
@@ -1,7 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import { get } from "../api/api.js";
+import { get, BASE_URL } from "../api/api.js";
+
+import "../../sass/_blocks.sass";
+
+const Textblock = ({ content }) => {
+  return (
+    <section className="block-text">
+      <h4>{content.preheadline}</h4>
+      <h2>{content.headline}</h2>
+      <article>{content.text}</article>
+    </section>
+  );
+};
+
+const Imageblock = ({ content }) => {
+  const imageUrl = content.image.length > 0 ? `${content.image[0].url}` : "";
+  return (
+    <section className="block-image">
+      <div className="left-side">
+        <img src={imageUrl} />
+      </div>
+      <div className="right-side">{content.text}</div>
+    </section>
+  );
+};
+
+const Imagegrid = ({ content }) => {
+  return (
+    <section className="block-imagegrid">
+      {content.image.map(img => {
+        return <img key={img.id} src={img.url} />;
+      })}
+    </section>
+  );
+};
 
 export default ({ id }) => {
   const [content, setContent] = useState({});
@@ -21,8 +55,20 @@ export default ({ id }) => {
       </Link>
       <h2>{content.title}</h2>
       <br />
-      {blocks.map(b => {
-        return <section key={b._uid}>{b._key}</section>;
+      {blocks.map(block => {
+        switch (block._key) {
+          case "textblock":
+            return <Textblock key={block._uid} content={block} />;
+            break;
+          case "imageblock":
+            return <Imageblock key={block._uid} content={block} />;
+            break;
+          case "imagegrid":
+            return <Imagegrid key={block._uid} content={block} />;
+          default:
+            return <section key={block._uid}>{block._key}</section>;
+            break;
+        }
       })}
     </div>
   );
