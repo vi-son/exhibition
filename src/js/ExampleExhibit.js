bf0bbce4fb5722eaca571f25def942a972fab6fd
Guido Schmidt <git@guidoschmidt.cc>
2020-09-23 23:21:36 +0200

    ADD/src/js/Wrapper.js: "frame" wrapper for content + artwork space

--- src/js/ExampleExhibit.js
+++ src/js/ExampleExhibit.js
@@ -12,20 +12,21 @@ const ExampleExhibit = ({ onCanvas, onMeta, onBack, meta }) => {
     const scene = new THREE.Scene();
     // Camera
     const camera = new THREE.PerspectiveCamera(
-      90,
+      30,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
     // Renderer
     const renderer = new THREE.WebGLRenderer({
-      canvas: canvasRef.current
+      canvas: canvasRef.current,
+      antialias: 1.0
     });
-    renderer.setClearColor(0xad9972, 1);
+    renderer.setClearColor(0x000000, 1);
     renderer.setSize(window.innerWidth, window.innerHeight);
     // Controls
     const controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.set(-2.3, 0, 2);
+    controls.target.set(0, 0, 0);
     camera.position.z = 1;
     camera.position.x = 2;
     controls.update();
@@ -33,7 +34,7 @@ const ExampleExhibit = ({ onCanvas, onMeta, onBack, meta }) => {
     const tooControls = new THREE.Vector3(0, 0, 0);
     const tooCamera = new THREE.Vector3(3, 3, 3);
     // Light
-    var light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+    var light = new THREE.HemisphereLight(0xffffbb, 0x080820, 3);
     scene.add(light);
     // Geometry
     var geometry = new THREE.BoxGeometry();
--- src/js/layouts/ExhibitionLayout.js
+++ src/js/layouts/ExhibitionLayout.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+import Wrapper from "../Wrapper.js";
 import Textblock from "./blocks/Textblock.js";
 import Imageblock from "./blocks/Imageblock.js";
 import Imagegrid from "./blocks/Imagegrid.js";
@@ -23,33 +24,35 @@ export default ({ id }) => {
   }, []);
 
   return (
-    <div className="layout-exhibition">
-      <Link className="button-back" to="/">
-        Zurück
-      </Link>
-      <br />
-      {blocks.map(block => {
-        switch (block._key) {
-          case "textblock":
-            return <Textblock key={block._uid} content={block} />;
-            break;
-          case "imageblock":
-            return <Imageblock key={block._uid} content={block} />;
-            break;
-          case "imagegrid":
-            return <Imagegrid key={block._uid} content={block} />;
-            break;
-          case "audioblock":
-            return <Audioblock key={block._uid} content={block} />;
-            break;
-          case "references":
-            return <Referencesblock key={block._uid} content={block} />;
-            break;
-          default:
-            return <section key={block._uid}>{block._key}</section>;
-            break;
-        }
-      })}
-    </div>
+    <Wrapper>
+      <div className="layout-exhibition">
+        <Link className="button-back" to="/">
+          Zurück
+        </Link>
+        <br />
+        {blocks.map(block => {
+          switch (block._key) {
+            case "textblock":
+              return <Textblock key={block._uid} content={block} />;
+              break;
+            case "imageblock":
+              return <Imageblock key={block._uid} content={block} />;
+              break;
+            case "imagegrid":
+              return <Imagegrid key={block._uid} content={block} />;
+              break;
+            case "audioblock":
+              return <Audioblock key={block._uid} content={block} />;
+              break;
+            case "references":
+              return <Referencesblock key={block._uid} content={block} />;
+              break;
+            default:
+              return <section key={block._uid}>{block._key}</section>;
+              break;
+          }
+        })}
+      </div>
+    </Wrapper>
   );
 };
