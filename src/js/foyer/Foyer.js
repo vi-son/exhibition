9ef57a79e9c44c216c5b5ae66560c73bc2cfedea
Guido Schmidt <git@guidoschmidt.cc>
2020-11-17 22:20:12 +0100

    EDIT/assets/mp3/foyer: update foyer music, improve interaction

--- src/js/foyer/Foyer.js
+++ src/js/foyer/Foyer.js
@@ -64,14 +64,16 @@ export default ({ exhibitions }) => {
     );
     var controls = new OrbitControls(camera, renderer.domElement);
     camera.position.set(0, 0, 12);
-    controls.autoRotate = false;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 0.5;
     controls.enableZoom = false;
     controls.enableDamping = true;
+    controls.enableRotate = false;
     controls.dampingFactor = 0.9;
 
     // Center sphere
     var geometry = new THREE.SphereGeometry(0.3, 32, 32);
-    var material = new THREE.MeshBasicMaterial({ color: 0x8a86bb });
+    var material = new THREE.MeshBasicMaterial({ color: 0x2b13ff });
     var centerSphere = new THREE.Mesh(geometry, material);
     scene.add(centerSphere);
 
@@ -84,13 +86,13 @@ export default ({ exhibitions }) => {
 
     // Foyer music
     const filePathFoyerMusic =
-      "/assets/mp3/foyer/foyer-backgorund-20201025.mp3";
+      "/assets/mp3/foyer/foyer-backgorund-20201117.mp3";
     var foyerMusic = new THREE.Audio(listener);
     // load a sound and set it as the Audio object's buffer
     audioLoader.load(filePathFoyerMusic, function(buffer) {
       foyerMusic.setBuffer(buffer);
       foyerMusic.setLoop(true);
-      foyerMusic.setVolume(0.75);
+      foyerMusic.setVolume(0.5);
       foyerMusic.play();
     });
 
@@ -115,7 +117,7 @@ export default ({ exhibitions }) => {
       var sphereGeometry = new THREE.SphereBufferGeometry(0.05, 32, 32);
       var sphere = new THREE.Mesh(
         sphereGeometry,
-        new THREE.MeshBasicMaterial(0x565469)
+        new THREE.MeshBasicMaterial(0x716ea0)
       );
       sphere.triggered = false;
       sphere.position.copy(position);
@@ -226,6 +228,7 @@ export default ({ exhibitions }) => {
           hit[0].object.scale.set(2.0, 2.0, 2.0);
           sounds.get(hit[0].object.id).play();
           playingSound = sounds.get(hit[0].object.id);
+          addLine(hit[0].object.position, hit[0].object);
           if (audioAnalyser === undefined) {
           }
         }
@@ -233,11 +236,22 @@ export default ({ exhibitions }) => {
         audioSpheres.children.forEach(a => {
           if (!a.triggered) {
             a.scale.set(1.0, 1.0, 1.0);
-            a.material.color.set(0x565469);
+            a.material.color.set(0x716ea0);
           }
         });
       }
     };
+
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+
+    function addLine(from, obj) {
+      const points = [];
+      points.push(new THREE.Vector3().sub(from).multiplyScalar(0.33));
+      points.push(new THREE.Vector3());
+      const geometry = new THREE.BufferGeometry().setFromPoints(points);
+      const line = new THREE.Line(geometry, lineMaterial);
+      obj.add(line);
+    }
 
     const clock = new THREE.Clock();
     var render = function() {
@@ -248,9 +262,9 @@ export default ({ exhibitions }) => {
       raycast();
 
       let dt = clock.getElapsedTime();
-      audioSpheres.rotation.y += 0.001;
-      audioSpheres.rotation.x += 0.00023;
-      audioSpheres.rotation.z += 0.0004;
+      audioSpheres.rotation.y += 0.00018;
+      audioSpheres.rotation.x += 0.00017;
+      audioSpheres.rotation.z += 0.00019;
 
       renderer.clear();
       renderer.render(backgroundPlane, backgroundCamera);
